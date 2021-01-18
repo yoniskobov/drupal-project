6e0208ba5ec34ec3859c7c8d99512dc29cdd4cf0
yoniskobov <38473834+yoniskobov@users.noreply.github.com>
2021-01-18 19:52:13 +0200

    low light indiq

---
+++ sites/default/files/js/js_RwN_qCzlVTW_2Xo6MctpTWMZ6wq7ZJCf_72nEOd0ID8.js
@@ -0,0 +1,315 @@
+/**
+ * @file
+ * JavaScript behaviors for CodeMirror integration.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  // @see http://codemirror.net/doc/manual.html#config
+  Drupal.webform = Drupal.webform || {};
+  Drupal.webform.codeMirror = Drupal.webform.codeMirror || {};
+  Drupal.webform.codeMirror.options = Drupal.webform.codeMirror.options || {};
+
+  /**
+   * Initialize CodeMirror editor.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.webformCodeMirror = {
+    attach: function (context) {
+      if (!window.CodeMirror) {
+        return;
+      }
+
+      // Webform CodeMirror editor.
+      $(context).find('textarea.js-webform-codemirror').once('webform-codemirror').each(function () {
+        var $input = $(this);
+
+        // Open all closed details, so that editor height is correctly calculated.
+        var $details = $input.parents('details:not([open])');
+        $details.attr('open', 'open');
+
+        // #59 HTML5 required attribute breaks hack for webform submission.
+        // https://github.com/marijnh/CodeMirror-old/issues/59
+        $input.removeAttr('required');
+
+        var options = $.extend({
+          mode: $input.attr('data-webform-codemirror-mode'),
+          lineNumbers: true,
+          lineWrapping: ($input.attr('wrap') !== 'off'),
+          viewportMargin: Infinity,
+          readOnly: !!($input.prop('readonly') || $input.prop('disabled')),
+          extraKeys: {
+            // Setting for using spaces instead of tabs - https://github.com/codemirror/CodeMirror/issues/988
+            Tab: function (cm) {
+              var spaces = Array(cm.getOption('indentUnit') + 1).join(' ');
+              cm.replaceSelection(spaces, 'end', '+element');
+            },
+            // On 'Escape' move to the next tabbable input.
+            // @see http://bgrins.github.io/codemirror-accessible/
+            Esc: function (cm) {
+              // Must show and then textarea so that we can determine
+              // its tabindex.
+              var textarea = $(cm.getTextArea());
+              $(textarea).show().addClass('visually-hidden');
+              var $tabbable = $(':tabbable');
+              var tabindex = $tabbable.index(textarea);
+              $(textarea).hide().removeClass('visually-hidden');
+
+              // Tabindex + 2 accounts for the CodeMirror's iframe.
+              $tabbable.eq(tabindex + 2).trigger('focus');
+            }
+
+          }
+        }, Drupal.webform.codeMirror.options);
+
+        var editor = CodeMirror.fromTextArea(this, options);
+
+        // Now, close details.
+        $details.removeAttr('open');
+
+        // Apply the textarea's min/max-height to the CodeMirror editor.
+        if ($input.css('min-height')) {
+          var minHeight = $input.css('min-height');
+          $(editor.getWrapperElement())
+            .css('min-height', minHeight)
+            .find('.CodeMirror-scroll')
+            .css('min-height', minHeight);
+        }
+        if ($input.css('max-height')) {
+          var maxHeight = $input.css('max-height');
+          $(editor.getWrapperElement())
+            .css('max-height', maxHeight)
+            .find('.CodeMirror-scroll')
+            .css('max-height', maxHeight);
+        }
+
+        // Issue #2764443: CodeMirror is not setting submitted value when
+        // rendered within a webform UI dialog or within an Ajaxified element.
+        var changeTimer = null;
+        editor.on('change', function () {
+          if (changeTimer) {
+            window.clearTimeout(changeTimer);
+            changeTimer = null;
+          }
+          changeTimer = setTimeout(function () {editor.save();}, 500);
+        });
+
+        // Update CodeMirror when the textarea's value has changed.
+        // @see webform.states.js
+        $input.on('change', function () {
+          editor.getDoc().setValue($input.val());
+        });
+
+        // Set CodeMirror to be readonly when the textarea is disabled.
+        // @see webform.states.js
+        $input.on('webform:disabled', function () {
+          editor.setOption('readOnly', $input.is(':disabled'));
+        });
+
+        // Delay refreshing CodeMirror for 500 millisecond while the dialog is
+        // still being rendered.
+        // @see http://stackoverflow.com/questions/8349571/codemirror-editor-is-not-loading-content-until-clicked
+        setTimeout(function () {
+          // Show tab panel and open details.
+          var $tabPanel = $input.parents('.ui-tabs-panel:hidden');
+          var $details = $input.parents('details:not([open])');
+
+          if (!$tabPanel.length && $details.length) {
+            return;
+          }
+
+          $tabPanel.show();
+          $details.attr('open', 'open');
+
+          editor.refresh();
+
+          // Hide tab panel and close details.
+          $tabPanel.hide();
+          $details.removeAttr('open');
+        }, 500);
+      });
+
+      // Webform CodeMirror syntax coloring.
+      if (window.CodeMirror.runMode) {
+        $(context).find('.js-webform-codemirror-runmode').once('webform-codemirror-runmode').each(function () {
+          // Mode Runner - http://codemirror.net/demo/runmode.html
+          CodeMirror.runMode($(this).addClass('cm-s-default').text(), $(this).attr('data-webform-codemirror-mode'), this);
+        });
+      }
+
+    }
+  };
+
+})(jQuery, Drupal);
+;
+/**
+* DO NOT EDIT THIS FILE.
+* See the following change record for more information,
+* https://www.drupal.org/node/2815083
+* @preserve
+**/
+
+(function ($, Drupal) {
+  function DetailsSummarizedContent(node) {
+    this.$node = $(node);
+    this.setupSummary();
+  }
+
+  $.extend(DetailsSummarizedContent, {
+    instances: []
+  });
+  $.extend(DetailsSummarizedContent.prototype, {
+    setupSummary: function setupSummary() {
+      this.$detailsSummarizedContentWrapper = $(Drupal.theme('detailsSummarizedContentWrapper'));
+      this.$node.on('summaryUpdated', $.proxy(this.onSummaryUpdated, this)).trigger('summaryUpdated').find('> summary').append(this.$detailsSummarizedContentWrapper);
+    },
+    onSummaryUpdated: function onSummaryUpdated() {
+      var text = $.trim(this.$node.drupalGetSummary());
+      this.$detailsSummarizedContentWrapper.html(Drupal.theme('detailsSummarizedContentText', text));
+    }
+  });
+  Drupal.behaviors.detailsSummary = {
+    attach: function attach(context) {
+      var $detailsElements = $(context).find('details').once('details');
+      DetailsSummarizedContent.instances = DetailsSummarizedContent.instances.concat($detailsElements.map(function (index, details) {
+        return new DetailsSummarizedContent(details);
+      }).get());
+    }
+  };
+  Drupal.DetailsSummarizedContent = DetailsSummarizedContent;
+
+  Drupal.theme.detailsSummarizedContentWrapper = function () {
+    return "<span class=\"summary\"></span>";
+  };
+
+  Drupal.theme.detailsSummarizedContentText = function (text) {
+    return text ? " (".concat(text, ")") : '';
+  };
+})(jQuery, Drupal);;
+/**
+* DO NOT EDIT THIS FILE.
+* See the following change record for more information,
+* https://www.drupal.org/node/2815083
+* @preserve
+**/
+
+(function ($, Drupal) {
+  Drupal.behaviors.detailsAria = {
+    attach: function attach() {
+      $('body').once('detailsAria').on('click.detailsAria', 'summary', function (event) {
+        var $summary = $(event.currentTarget);
+        var open = $(event.currentTarget.parentNode).attr('open') === 'open' ? 'false' : 'true';
+        $summary.attr({
+          'aria-expanded': open,
+          'aria-pressed': open
+        });
+      });
+    }
+  };
+})(jQuery, Drupal);;
+/**
+* DO NOT EDIT THIS FILE.
+* See the following change record for more information,
+* https://www.drupal.org/node/2815083
+* @preserve
+**/
+
+(function ($, Modernizr, Drupal) {
+  function CollapsibleDetails(node) {
+    this.$node = $(node);
+    this.$node.data('details', this);
+    var anchor = window.location.hash && window.location.hash !== '#' ? ", ".concat(window.location.hash) : '';
+
+    if (this.$node.find(".error".concat(anchor)).length) {
+      this.$node.attr('open', true);
+    }
+
+    this.setupSummaryPolyfill();
+  }
+
+  $.extend(CollapsibleDetails, {
+    instances: []
+  });
+  $.extend(CollapsibleDetails.prototype, {
+    setupSummaryPolyfill: function setupSummaryPolyfill() {
+      var $summary = this.$node.find('> summary');
+      $('<span class="details-summary-prefix visually-hidden"></span>').append(this.$node.attr('open') ? Drupal.t('Hide') : Drupal.t('Show')).prependTo($summary).after(document.createTextNode(' '));
+      $('<a class="details-title"></a>').attr('href', "#".concat(this.$node.attr('id'))).prepend($summary.contents()).appendTo($summary);
+      $summary.append(this.$summary).on('click', $.proxy(this.onSummaryClick, this));
+    },
+    onSummaryClick: function onSummaryClick(e) {
+      this.toggle();
+      e.preventDefault();
+    },
+    toggle: function toggle() {
+      var _this = this;
+
+      var isOpen = !!this.$node.attr('open');
+      var $summaryPrefix = this.$node.find('> summary span.details-summary-prefix');
+
+      if (isOpen) {
+        $summaryPrefix.html(Drupal.t('Show'));
+      } else {
+        $summaryPrefix.html(Drupal.t('Hide'));
+      }
+
+      setTimeout(function () {
+        _this.$node.attr('open', !isOpen);
+      }, 0);
+    }
+  });
+  Drupal.behaviors.collapse = {
+    attach: function attach(context) {
+      if (Modernizr.details) {
+        return;
+      }
+
+      var $collapsibleDetails = $(context).find('details').once('collapse').addClass('collapse-processed');
+
+      if ($collapsibleDetails.length) {
+        for (var i = 0; i < $collapsibleDetails.length; i++) {
+          CollapsibleDetails.instances.push(new CollapsibleDetails($collapsibleDetails[i]));
+        }
+      }
+    }
+  };
+
+  var handleFragmentLinkClickOrHashChange = function handleFragmentLinkClickOrHashChange(e, $target) {
+    $target.parents('details').not('[open]').find('> summary').trigger('click');
+  };
+
+  $('body').on('formFragmentLinkClickOrHashChange.details', handleFragmentLinkClickOrHashChange);
+  Drupal.CollapsibleDetails = CollapsibleDetails;
+})(jQuery, Modernizr, Drupal);;
+/**
+ * @file
+ * JavaScript behaviors for select menu.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Disable select menu options using JavaScript.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.webformSelectOptionsDisabled = {
+    attach: function (context) {
+      $('select[data-webform-select-options-disabled]', context).once('webform-select-options-disabled').each(function () {
+        var $select = $(this);
+        var disabled = $select.attr('data-webform-select-options-disabled').split(/\s*,\s*/);
+        $select.find('option').filter(function isDisabled() {
+          return ($.inArray(this.value, disabled) !== -1);
+        }).attr('disabled', 'disabled');
+      });
+    }
+  };
+
+
+})(jQuery, Drupal);
+;
